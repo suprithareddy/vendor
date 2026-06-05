7d02b80752f50650ec9807c25413a45654181654
Supritha <supritha.ravipalli@gmail.com>
2026-06-05 23:04:31 +0530

    Initial commit

---
+++ src/main/java/com/vendor/vendorservice/VendorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vendor.vendorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendor/vendorservice/VendorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendor.vendorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
